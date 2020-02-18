5c63c76c226d1228f079658b01d917d25ef21ae8
Ryan Borja <rcborja.school@gmail.com>
2020-02-17 18:54:22 -0600

    fixing file tree

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
+
 const express = require("express");
-// const logger = require("morgan");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
-// const db = require("./models/user");
-// const db = require("./models");
 const app = express();
 
-// app.use(logger("dev"));
+const db = require("./models");
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-// require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
-require("./backend/routes.js")(app);
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 
 app.listen(PORT, () => {
-  console.log(`App running on http://localhost:${PORT} !`);
+    console.log(`App running on http://localhost${PORT}`);
 });
